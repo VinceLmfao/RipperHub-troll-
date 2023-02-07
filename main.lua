311031877b8186b39a9be259203c0814350aeb10
Vince <98156995+VinceLmfao@users.noreply.github.com>
2023-02-07 09:28:39 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,8 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-    loadstring(game:HttpGet('https://pastebin.com/raw/AtCcfMYZ'))()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/RipperHub/main/RipperHubV3.lua'))();
 elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 then
-    loadstring(game:HttpGet('https://pastebin.com/raw/AtCcfMYZ'))()
+   
     loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/RipperHub/main/KL.lua'))();
 elseif game.PlaceId == 855499080 then
-    loadstring(game:HttpGet('https://pastebin.com/raw/AtCcfMYZ'))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xDestinyx/RipperHub/main/SkyWars.lua"))();
 end
